f6ee3cdeade0985ee8a0b8364a84f01d23440579
YianChen <ianchen0419@gmail.com>
2019-02-02 22:37:15 +0800

    top

--- inc/js/base.js
+++ inc/js/base.js
@@ -1,41 +1,42 @@
-// var nowPath=location.pathname.split('/').pop();
+var nowPath=location.pathname.split('/').pop();
 
-// //header.html load
-// var headerXhr=new XMLHttpRequest();
+//header.html load
+var headerXhr=new XMLHttpRequest();
 
-// headerXhr.open('GET', 'header.html', true);
-// headerXhr.send();
+headerXhr.open('GET', 'header.html', true);
+headerXhr.send();
 
-// headerXhr.onreadystatechange=function(){
-// 	if(headerXhr.readyState==4 && headerXhr.status==200){
-// 		header.innerHTML = headerXhr.responseText;
-// 		menuHighlight();
-// 	}
-// };
+headerXhr.onreadystatechange=function(){
+	if(headerXhr.readyState==4 && headerXhr.status==200){
+		header.innerHTML = headerXhr.responseText;
+		menuHighlight();
+	}
+};
 
 
-// //footer.html load
-// var footerXhr=new XMLHttpRequest();
+//footer.html load
+var footerXhr=new XMLHttpRequest();
 
-// footerXhr.open('GET', 'footer.html', true);
-// footerXhr.send();
+footerXhr.open('GET', 'footer.html', true);
+footerXhr.send();
 
-// footerXhr.onreadystatechange=function(){
-// 	if(footerXhr.readyState==4 && footerXhr.status==200){
-// 		footer.innerHTML = footerXhr.responseText;
-// 	}
-// };
+footerXhr.onreadystatechange=function(){
+	if(footerXhr.readyState==4 && footerXhr.status==200){
+		footer.innerHTML = footerXhr.responseText;
+	}
+};
 
-// //menu highlight
-// function menuHighlight(){
-
-// 	var visitedMenu=document.querySelector('nav a[href*="' + nowPath + '"]');
-
-// 	if(visitedMenu){
-// 		visitedMenu.closest('li').classList.add('active');
-// 	}else{
-// 		document.querySelector('nav ul li').classList.add('active');
-// 	}
-	
-// }
+//menu highlight
+function menuHighlight(){
+	var menuList=document.querySelectorAll('nav li');
+	if(nowPath==='' || nowPath==='index.html'){
+		menuList[0].classList.add('active');
+	}else if(nowPath==='products.html' || nowPath==='miraitoukei.html' || nowPath==='asahiru.html' || nowPath==='dogadehazard.html' || nowPath==='locatioin-passport.html' || nowPath==='roadinfo.html' || nowPath.indexOf('miraishoken')===0 || nowPath==='act-terms.html' || nowPath==='privacy-policy.html'){
+		menuList[1].classList.add('active');
+	}else if(nowPath==='company.html' || nowPath==='outline.html' || nowPath==='message.html' || nowPath==='books.html'){
+		menuList[2].classList.add('active');
+	}else if(nowPath==='blog.html'){
+		menuList[3].classList.add('active');
+	}
+}
 
